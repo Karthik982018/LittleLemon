04161e1a27d254660d051199c6fc65886a3d566b
Karthik <vskarthik982018@gmail.com>
2023-05-08 16:59:49 +0100

    added logo.svg and navigation

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,8 @@
+import Logo from '../../Assets/Logo.svg'
 const Header=(props)=>{
     return (
         <header>
-
+         <img src={Logo}alt="logo" style={{width: "100px", height: "50px"}}></img>
         </header>
     );
 }
--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,6 +1,14 @@
 const Navigation=(props)=>{
     return (
         <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">About</a> </li>
+                <li><a href="/">Menu</a></li>
+                <li><a href="/">Reservations</a></li>
+                <li><a  href="/">Order Online</a></li>
+                <li><a href="/">Login</a></li>
+            </ul>
 
         </nav>
     );
